7bd736ff25adf96eb8649650a14458bdfbf1f15c
makefu <github@syntax-fehler.de>
2016-05-10 17:48:24 +0200

    update bintray and travis

--- bintray.sh
+++ bintray.sh
@@ -10,23 +10,23 @@ API=https://api.bintray.com
 if [ -z "TRAIT" ]; then
     echo "Need to set TRAIT"
     exit 1
-fi  
-TRAIT_URL="https://github.com/probonopd/minikrebs/tree/master/"$(find traits/ -name "$TRAIT" -type d)
+fi
+TRAIT_URL="https://github.com/krebscode/minikrebs/tree/master/"$(find traits/ -name "$TRAIT" -type d)
 echo "$TRAIT_URL"
 
-# Get description from trait README if it exists
-DESCRIPTION=$(cat $(find traits -type f -wholename "*$TRAIT/READM*")) || true
-
+DESCRIPTION="$(cat "profiles/$TRAIT" | python -c 'import json,sys; print json.dumps(sys.stdin.read())')"
+echo "$DESCRIPTION"
 FILE=$1
 [ -f "$FILE" ] || exit 1
 
-BINTRAY_USER="probono"
+BINTRAY_USER="makefu"
+BINTRAY_SUBJECT="krebscode"
 BINTRAY_API_KEY=$BINTRAY_API_KEY # env
-BINTRAY_REPO="OpenWrt"
+BINTRAY_REPO="minikrebs"
 PCK_NAME=$TRAIT
 WEBSITE_URL="$TRAIT_URL"
-ISSUE_TRACKER_URL="https://github.com/probonopd/minikrebs/issues"
-VCS_URL="https://github.com/probonopd/minikrebs.git" # Mandatory for packages in free Bintray repos
+ISSUE_TRACKER_URL="https://github.com/krebscode/minikrebs/issues"
+VCS_URL="https://github.com/krebscode/minikrebs.git" # Mandatory for packages in free Bintray repos
 
 which curl || exit 1
 which grep || exit 1
@@ -63,7 +63,7 @@ echo ""
 echo "Creating package ${PCK_NAME}..."
     data="{
     \"name\": \"${PCK_NAME}\",
-    \"desc\": \"${DESCRIPTION}\",
+    \"desc\": ${DESCRIPTION},
     \"desc_url\": \"auto\",
     \"website_url\": [\"${WEBSITE_URL}\"],
     \"vcs_url\": [\"${VCS_URL}\"],
@@ -71,11 +71,11 @@ echo "Creating package ${PCK_NAME}..."
     \"licenses\": [\"MIT\"],
     \"labels\": [\"AppImage\", \"AppImageKit\"]
     }"
-${CURL} -X POST -d "${data}" ${API}/packages/${BINTRAY_USER}/${BINTRAY_REPO}
+${CURL} -X POST -d "${data}" ${API}/packages/${BINTRAY_SUBJECT}/${BINTRAY_REPO}
 
 echo ""
 echo "Uploading and publishing ${FILE}..."
-${CURL} -T ${FILE} "${API}/content/${BINTRAY_USER}/${BINTRAY_REPO}/${PCK_NAME}/${VERSION}/$(basename ${FILE})?publish=1&override=1"
+${CURL} -T ${FILE} "${API}/content/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${PCK_NAME}/${VERSION}/$(basename ${FILE})?publish=1&override=1"
 
 if [ $(env | grep TRAVIS_JOB_ID ) ] ; then
 echo ""
@@ -87,7 +87,6 @@ BUILD_LOG="https://api.travis-ci.org/jobs/${TRAVIS_JOB_ID}/log.txt?deansi=true"
     "content": "'${BUILD_LOG}'"
   }
 }'
-${CURL} -X POST -d "${data}" ${API}/packages/${BINTRAY_USER}/${BINTRAY_REPO}/${PCK_NAME}/versions/${VERSION}/release_notes
+${CURL} -X POST -d "${data}" ${API}/packages/${BINTRAY_SUBJECT}/${BINTRAY_REPO}/${PCK_NAME}/versions/${VERSION}/release_notes
 fi
-
 
