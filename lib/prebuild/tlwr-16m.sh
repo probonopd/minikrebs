17c4550b08a190c58d4ab97608f46220f2197a0f
makefu <github@syntax-fehler.de>
2016-09-07 22:53:13 +0200

    test tlwr-16m

--- lib/prebuild/tlwr-16m.sh
+++ lib/prebuild/tlwr-16m.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-set -euf
+set -xeuf
+echo $PWD
+ls 
+test -e target/linux/ar71xx/image/Makefile
 sed -i 's#Device/tplink-4mlzma#Device/tplink-16mlzma#'   target/linux/ar71xx/image/Makefile
