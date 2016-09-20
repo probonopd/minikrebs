e3dc5d453b3823bec634484037f877d446b56df3
makefu <github@syntax-fehler.de>
2016-09-20 23:25:40 +0200

    initial vomit of keepalive-hub base files

--- traits/special_purpose/keepalive-hub/files/usr/bin/keepalived-check.sh
+++ traits/special_purpose/keepalive-hub/files/usr/bin/keepalived-check.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 HUB=uhub
+
+set -ef
 if ! pidof $HUB >/dev/null; then
   echo "$HUB is dead"
   exit 1
 fi
+. /lib/functions/network.sh
+network_get_ipaddr ip wan
+if test -z "$ip"; then
+	echo "WAN has no ip address assigned"
+fi
+
--- traits/special_purpose/keepalive-hub/files/usr/bin/keepalived-notify.sh
+++ traits/special_purpose/keepalive-hub/files/usr/bin/keepalived-notify.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-LOGNAME=keepalived irc-announce "I've switched to $3 state" 2>&1 | tee /tmp/irc-log
 logger "$@"
+STATUS=
 case $3 in
   FAULT)
+	STATUS=": we are fucked i suppose?"
+	# TODO: maybe restart system
     ;;
   BACKUP)
+	STATUS=": Restarted uhub to force user reconnection (in background)"
+	(/etc/init.d/uhub stop; sleep 20; /etc/init.d/uhub start) &
     ;;
   MASTER)
+	STATUS=": Updated hub.nsupdate.info - $(update-hub-dns.sh)"
     ;;
 esac
+
+irc-announce "I've switched to $3 state $STATUS" >/dev/null 2>&1
